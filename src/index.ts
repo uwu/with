b6b62e5842107d6131db37c92d5b9197703e868d
Kyza <kyza@kyza.net>
2022-11-16 16:35:24 -0500

    stuff

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,6 @@ export function getFunctionBody(functionString: string): string | null {
 	// () => (...)
 	// () => ...
 	else if (body.startsWith("=>")) {
-		console.log("2");
 		return `return (${body.slice(2)})`;
 	}
 
