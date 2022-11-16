632405c1d73f915dec60534c09776bc8024b0bcd
Kyza <kyza@kyza.net>
2022-11-16 01:41:19 -0500

    Add lifter and Proxy context validator, switch to reliable comment slice

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,48 @@
-export default function withWith<T, Return>(
+export default function withWith<
+	T extends object,
+	Lifter extends (variable?: string) => unknown,
+	Return
+>(
 	scope: T,
-	cb: () => (scope: T) => Return,
-	binding?: object
+	cb: (lifter?: Lifter) => (scope: T) => Return,
+	options?: {
+		lifter?: Lifter;
+		binding?: object;
+	}
 ): Return {
-	let cbString = cb().toString();
+	let cbString = cb(options?.lifter).toString();
 
-	if (cbString.match(/{/gi).length > 1) {
-		cbString = cbString.replace(/{.*?}/, "");
+	if (cbString.indexOf("/*$WITHSTART$*/")) {
+		cbString = cbString.slice(
+			cbString.indexOf("/*$WITHSTART$*/"),
+			cbString.lastIndexOf("}")
+		);
 	}
 
-	cbString = cbString.slice(
-		cbString.indexOf("{") + 1,
-		cbString.lastIndexOf("}")
-	);
-
 	return new Function(`with(arguments[0]){${cbString}}`).bind(
-		binding ?? globalThis
-	)(scope);
+		options?.binding ?? globalThis
+	)(
+		new Proxy(scope, {
+			has(target, property) {
+				if (Reflect.has(target, property)) return true;
+				if (
+					"lifter" in (options ?? {}) &&
+					typeof property === "string" &&
+					options?.lifter?.(property) !== undefined
+				) {
+					return true;
+				}
+				return false;
+			},
+			get(target, property, receiver) {
+				if (
+					typeof property === "string" &&
+					!Object.hasOwn(target, property)
+				) {
+					return options?.lifter?.(property);
+				}
+				return Reflect.get(target, property, receiver);
+			},
+		})
+	);
 }
